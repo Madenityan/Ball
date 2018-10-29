26b1d5ab08b5e0f1c91ac1fa5d5b50249a834b7b
Madenityan <marinakorsyn@gmail.com>
2018-10-29 23:57:24 +0200

    changed css to js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+let myBall = document.getElementById('ball');
+let startPosition = 0;
 
+let timer = setInterval(move,10);
 
+function move() {
+    let top = myBall.offsetTop;
+
+    if (top < 500) {
+        myBall.style.top = top + 10 + 'px';
+    }
+
+}
 
