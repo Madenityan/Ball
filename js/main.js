eb45827fde839f6c248291f78095afc4449a1c22
Madenityan <marinakorsyn@gmail.com>
2018-10-30 14:29:13 +0200

    fixed a styles and added js for a ball

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
-let myBall = document.getElementById('ball');
-let startPosition = 0;
 
-let timer = setInterval(move,10);
+let delay = 50,
+    i = 0;
 
-function move() {
-    let top = myBall.offsetTop;
+let startTimer = function (pixels) {
+    let myBall = document.getElementById('ball');
+    let bottom = myBall.offsetTop;
 
-    if (top < 500) {
-        myBall.style.top = top + 10 + 'px';
+    if ((pixels > 0 && bottom > 250)|| (pixels < 0 && bottom < 90) ) {
+        clearInterval(timer);
+
+         timer = setInterval(function () {
+            startTimer(pixels * -1)
+        }, delay);
+
     }
+        myBall.style.top = bottom + pixels + 'px';
+        i++;
+};
 
-}
+let timer = setInterval(function(){
+    startTimer(20)
+}, delay);
 
