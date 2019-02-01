c8d69fb962cf85fdabb321982b56e9ac956f88ba
Madenityan <marinakorsyn@gmail.com>
2019-02-01 10:49:29 +0200

    fixed a code style

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
 let delay = 50,
-    i = 0;
+  i = 0;
 
 let startTimer = function(pixels) {
-    let myBall = document.getElementById('ball');
-    let bottom = myBall.offsetTop;
+  let myBall = document.getElementById('ball');
+  let bottom = myBall.offsetTop;
 
-    if ((pixels > 0 && bottom > 250) || (pixels < 0 && bottom < 90)) {
-        clearInterval(timer);
+  if ((pixels > 0 && bottom > 250) || (pixels < 0 && bottom < 90)) {
+    clearInterval(timer);
 
-         timer = setInterval(function() {
-            startTimer(pixels * -1)
-        }, delay);
-    }
-    myBall.style.top = bottom + pixels + 'px';
-    i++;
+     timer = setInterval(function() {
+        startTimer(pixels * -1)
+    }, delay);
+  }
+  myBall.style.top = bottom + pixels + 'px';
+  i++;
 };
 
 let timer = setInterval(function() {
-    startTimer(20)
+  startTimer(20)
 }, delay);
 
